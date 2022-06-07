e0e807576b38660580e587cc8c3215fb122ff22b
arora <aroramadhav65@gmail.com>
2022-06-07 19:23:19 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:little_leap_clone/Screens/Splash_Screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp(),);
 }
 
 class MyApp extends StatelessWidget {
