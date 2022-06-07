d0fbd118f6c68e5e05ba7c795ed1bbb40eceaa79
arora <aroramadhav65@gmail.com>
2022-06-07 20:43:50 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -33,47 +33,57 @@ class _NavigationState extends State<Navigation> {
     //       snapshot.hasData &&
     //       snapshot.data != ConnectivityResult.none) {
     return Scaffold(
-        bottomNavigationBar: GNav(
-            rippleColor:
-                Colors.grey.shade800, // tab button ripple color when pressed
-            hoverColor: Colors.grey.shade700, // tab button hover color
-            haptic: true, // haptic feedback
-            tabBorderRadius: 15,
-            tabActiveBorder:
-                Border.all(color: Colors.black, width: 1), // tab button border
-            tabBorder:
-                Border.all(color: Colors.grey, width: 1), // tab button border
-            tabShadow: [
-              BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)
-            ], // tab button shadow
-            curve: Curves.easeOutExpo, // tab animation curves
-            duration: Duration(milliseconds: 900), // tab animation duration
-            gap: 8, // the tab button gap between icon and text
-            color: Colors.grey[800], // unselected icon color
-            activeColor: Colors.purple, // selected icon and text color
-            iconSize: 24, // tab button icon size
-            tabBackgroundColor:
-                Colors.purple.withOpacity(0.1), // selected tab background color
-            padding: EdgeInsets.symmetric(
-                horizontal: 20, vertical: 5), // navigation bar padding
-            tabs: [
-              GButton(
-                icon: Icons.home_outlined,
-                text: 'Home',
-              ),
-              GButton(
-                icon: Icons.videocam_outlined,
-                text: 'Likes',
-              ),
-              GButton(
-                icon: Icons.leaderboard_outlined,
-                text: 'Search',
-              ),
-              GButton(
-                icon: Icons.menu,
-                text: 'Profile',
-              )
-            ])
+        bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: Colors.black,
+              tabs: [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: LineIcons.heart,
+                  text: 'Likes',
+                ),
+                GButton(
+                  icon: LineIcons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
         // CurvedNavigationBar(
         //   key: _bottomNavigationKey,
         //   index: 0,
