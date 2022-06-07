8bb53dfef57bdb712ae00d2d3ed8f9582d1bdab7
arora <aroramadhav65@gmail.com>
2022-06-07 19:07:22 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class name extends StatefulWidget {
+  name({Key? key}) : super(key: key);
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
