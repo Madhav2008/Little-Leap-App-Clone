4c5d8ef7385c9b9e361744b814d5c09d03660d94
arora <aroramadhav65@gmail.com>
2022-06-07 20:27:27 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -45,7 +45,7 @@ class _NavigationState extends State<Navigation> {
           Icon(
             Icons.videocam_outlined,
             size: 30,
-            color: (_page == 1) ? Colors.greenAccent : Colors.white,
+            color: (_page == 1) ? Colors.green : Colors.white,
           ),
           Icon(
             Icons.leaderboard_outlined,
