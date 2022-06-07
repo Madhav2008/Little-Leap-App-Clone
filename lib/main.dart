df90932d256770b5a06e4a97366296fce5750a15
arora <aroramadhav65@gmail.com>
2022-06-07 19:12:50 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:little_leap_clone/Screens/Login_Screen.dart';
+import 'package:little_leap_clone/Screens/Splash_Screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: SplashScreen(),
     );
   }
 }
