675c14b67d6fdde38296fe4bf9b9444f8c36338f
arora <aroramadhav65@gmail.com>
2022-06-07 20:38:42 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -1,6 +1,7 @@
 // import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:little_leap_clone/Screens/Home_Screen.dart';
 import 'package:little_leap_clone/Screens/Leaderboard.dart';
 import 'package:little_leap_clone/Screens/Live_Classes.dart';
