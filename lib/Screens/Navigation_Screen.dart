24d293f75061ffe25b8cf230b0c45d972dd18a4a
arora <aroramadhav65@gmail.com>
2022-06-07 20:38:32 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -33,6 +33,41 @@ class _NavigationState extends State<Navigation> {
     //       snapshot.data != ConnectivityResult.none) {
     return Scaffold(
       bottomNavigationBar: 
+      GNav(
+  rippleColor: Colors.grey[800], // tab button ripple color when pressed
+  hoverColor: Colors.grey[700], // tab button hover color
+  haptic: true, // haptic feedback
+  tabBorderRadius: 15, 
+  tabActiveBorder: Border.all(color: Colors.black, width: 1), // tab button border
+  tabBorder: Border.all(color: Colors.grey, width: 1), // tab button border
+  tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)], // tab button shadow
+  curve: Curves.easeOutExpo, // tab animation curves
+  duration: Duration(milliseconds: 900), // tab animation duration
+  gap: 8, // the tab button gap between icon and text 
+  color: Colors.grey[800], // unselected icon color
+  activeColor: Colors.purple, // selected icon and text color
+  iconSize: 24, // tab button icon size
+  tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
+  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
+  tabs: [
+    GButton(
+      icon: LineIcons.home,
+      text: 'Home',
+    ),
+    GButton(
+      icon: LineIcons.heart_o,
+      text: 'Likes',
+    ),
+    GButton(
+      icon: LineIcons.search,
+      text: 'Search',
+    ),
+    GButton(
+      icon: LineIcons.user,
+      text: 'Profile',
+    )
+  ]
+)
       // CurvedNavigationBar(
       //   key: _bottomNavigationKey,
       //   index: 0,
