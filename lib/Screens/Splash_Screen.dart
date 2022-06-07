117fb5606b55a21860e1a82614b7172b31fe7024
arora <aroramadhav65@gmail.com>
2022-06-07 19:13:36 +0530

    Update

--- lib/Screens/Splash_Screen.dart
+++ lib/Screens/Splash_Screen.dart
@@ -1,15 +1,88 @@
+import 'dart:async';
+import 'package:Foodybite/auth/wrapper.dart';
+import 'package:Foodybite/screens/splash_screen/Zig_Zag_Zipper.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
-  SplashScreen({Key? key}) : super(key: key);
-
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  _SplashScreenState createState() => new _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Scaffold(
+        backgroundColor: Colors.redAccent,
+        body: new Stack(
+          children: <Widget>[
+            new Positioned(
+              child: ClipPath(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2,
+                  color: Color.fromRGBO(226, 55, 68, 1.0),
+                ),
+                clipper: ZigZagClipper(),
+              ),
+            ),
+            new Positioned(
+              top: 40,
+              child: ClipPath(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2,
+                  color: Color.fromRGBO(226, 55, 68, 0.80),
+                ),
+                clipper: ZigZagClipper(),
+              ),
+            ),
+            new Positioned(
+              top: 80,
+              child: ClipPath(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2,
+                  color: Color.fromRGBO(226, 55, 68, 0.60),
+                ),
+                clipper: ZigZagClipper(),
+              ),
+            ),
+            new Center(
+              child: Image.asset(
+                "assets/images/Logo4Recipo.png",
+                width: 500,
+                height: 500,
+              )
+            )
+          ],
+        ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  void navigationPage() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token') ?? '';
+    if (token.isEmpty) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Wrapper()),
+      );
+    }else{
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Wrapper()),
+      );
+    }
   }
 }
