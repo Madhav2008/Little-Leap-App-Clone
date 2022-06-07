c3ec27b524d70774bb77383c132edb9cfa4e4a5a
arora <aroramadhav65@gmail.com>
2022-06-07 19:19:33 +0530

    Update

--- lib/Screens/Splash_Screen.dart
+++ lib/Screens/Splash_Screen.dart
@@ -42,7 +42,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 clipper: ZigZagClipper(),
               ),
             ),
-            new Positioned(
+            Positioned(
               top: 80,
               child: ClipPath(
                 child: Container(
