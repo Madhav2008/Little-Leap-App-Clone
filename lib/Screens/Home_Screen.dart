a72e4753edc07b222de73b26a7a06b34a2547e9b
arora <aroramadhav65@gmail.com>
2022-06-08 19:32:23 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -96,21 +96,6 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ),
                 ),
-                // child: Stack(
-                //   children: [
-                //     Container(
-                //       width: MediaQuery.of(context).size.width,
-                //       height: MediaQuery.of(context).size.height,
-                //       child: Opacity(
-                //         opacity: 0.2,
-                //         child: Image.asset(
-                //           '../assets/images/back.jpg',
-                //           fit: BoxFit.cover,
-                //         ),
-                //       ),
-                //     ),
-                //   ],
-                // ),
               ),
             ),
           ],
