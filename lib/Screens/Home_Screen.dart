87b672e04b2c7821e83eeadaa809b39951b47d29
arora <aroramadhav65@gmail.com>
2022-06-08 19:59:40 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -118,7 +118,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ],
                         ),
                         child: ListTile(
-                          title: Text(''),
+                          title: Text('Daily challenges'),
                         ),
                       ),
                     ],
