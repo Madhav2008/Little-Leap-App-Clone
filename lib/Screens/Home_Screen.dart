2769c2de5664fbf7963bef413f027d29b7f22ac2
arora <aroramadhav65@gmail.com>
2022-06-08 19:12:09 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -84,10 +84,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(50),
                     topRight: Radius.circular(50),
-                  ),image: Image.asset(
-                          '../assets/images/back.jpg',
-                          fit: BoxFit.cover,
-                        ),
+                  ),image: DecorationImage
                 ),
                 child: Stack(
                   children: [
