6d34af5910e6f8e7db51abe589ecd9af3c4a4521
arora <aroramadhav65@gmail.com>
2022-06-08 17:43:48 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -78,7 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: const EdgeInsets.only(top: 32),
               child: Container(
-                height: MediaQuery.of(context).size.height - 173,
+                height: MediaQuery.of(context).size.height - 173.5,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
