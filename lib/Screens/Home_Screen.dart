6869e1782872c364d213a0eb77110b5deccbf049
arora <aroramadhav65@gmail.com>
2022-06-08 19:39:17 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -101,8 +101,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     Container(
                       width: MediaQuery.of(context).size.width - 100,
                       height: 200,
-                      color: Colors.white,
                       decoration: BoxDecoration(
+                      color: Colors.white,
                         border: Border.all(),
                       ),
                     ),
