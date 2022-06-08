db944ebe68533b35ea249d0699cd982fcdbab1f6
arora <aroramadhav65@gmail.com>
2022-06-08 17:14:26 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -17,7 +17,10 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 15, left: 20,),
+              padding: const EdgeInsets.only(
+                top: 15,
+                left: 20,
+              ),
               child: ListTile(
                 leading: CircleAvatar(
                   backgroundImage: AssetImage('../assets/images/My.png'),
