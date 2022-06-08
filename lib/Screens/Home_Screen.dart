a954006dd8e5d04c613ee0d75a8ebbfc2ef8d191
arora <aroramadhav65@gmail.com>
2022-06-08 19:28:55 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -88,7 +88,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   image: DecorationImage(
                     colorFilter: ColorFilter.mode(
                       Colors.white,
-                      BlendMode.colorBurn,
+                      BlendMode.dst,
                     ),
                     fit: BoxFit.cover,
                     image: AssetImage(
