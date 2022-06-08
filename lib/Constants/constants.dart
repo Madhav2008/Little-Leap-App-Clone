fed773255c484f47624399bc241d7771b751c3f0
arora <aroramadhav65@gmail.com>
2022-06-08 22:58:40 +0530

    Update

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -6,4 +6,4 @@ const lightPink = Color(0xFFF16991);
 const lightBlue = Color(0xFF4AD6FD);
 const purple = Color(0xFF7944B7);
 const pink = Color(0xFFEE4476);
-const blue = Color(0xFF2aD6FD);
+const blue = Color(0xFF4BD6FD);
