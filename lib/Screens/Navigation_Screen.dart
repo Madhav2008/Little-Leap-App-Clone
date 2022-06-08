c4594578927ebbbbea6e0a031c5670c6ab5fc499
arora <aroramadhav65@gmail.com>
2022-06-08 19:31:47 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -34,7 +34,7 @@ class _NavigationState extends State<Navigation> {
     //       snapshot.hasData &&
     //       snapshot.data != ConnectivityResult.none) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey.shade300,
       body: Center(
         child: _tabItems.elementAt(_selectedIndex),
       ),
