ed34ebeec78ec73e37f6ef4657d19d26e03a3019
arora <aroramadhav65@gmail.com>
2022-06-08 13:27:45 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -11,7 +11,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green.shade400,
+      backgroundColor: Color(0xFF42CB7E),
       body: Container(
         child: Padding(
           padding: const EdgeInsets.only(top: 100.0),
