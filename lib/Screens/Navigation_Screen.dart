9188364ded6a2494c2364dc0d698bd76302f2766
arora <aroramadhav65@gmail.com>
2022-06-08 19:21:14 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -52,7 +52,7 @@ class _NavigationState extends State<Navigation> {
           ],
           image: DecorationImage(
             opacity: 1,
-            colorFilter: ColorFilter.mode(Colors.white, blendMode),
+            colorFilter: ColorFilter.mode(Colors.red, blendMode),
             fit: BoxFit.cover,
             image: AssetImage(
               '../assets/images/back.jpg',
