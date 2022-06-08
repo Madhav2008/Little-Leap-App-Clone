83ab0907018b9266676d4235a3ba592d4b4006be
arora <aroramadhav65@gmail.com>
2022-06-08 17:14:59 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               child: ListTile(
                 leading: CircleAvatar(
-                  backgroundImage: AssetImage('../assets/images/My.png'),
+                  backgroundImage: AssetImage('../assets/images/My.png'),radius: 30,
                 ),
               ),
             ),
