2271fc0b48ad93212a4d3462b8dd252183d303b6
arora <aroramadhav65@gmail.com>
2022-06-08 13:18:42 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -32,7 +32,8 @@ class _NavigationState extends State<Navigation> {
     //   if (snapshot != null &&
     //       snapshot.hasData &&
     //       snapshot.data != ConnectivityResult.none) {
-    return Scaffold(backgroundColor: Colors.white,
+    return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 20,
         title: const Text('GoogleNavBar'),
