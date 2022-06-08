4cec66b6aae1cc612a72dcf9bb2b48cd8c2283d7
arora <aroramadhav65@gmail.com>
2022-06-08 17:25:48 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -41,7 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     color: Colors.grey.shade200,
                   ),
                 ),
-                trailing: Container(child: Row,),
+                trailing: Container(child: Row(),),
               ),
             ),
             Padding(
