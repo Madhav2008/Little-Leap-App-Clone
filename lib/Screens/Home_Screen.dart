d9fc11192b260ab183f6b35c7c3f06fcffff59d0
arora <aroramadhav65@gmail.com>
2022-06-08 19:36:12 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -96,6 +96,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ),
                 ),
+                child: Column(),
               ),
             ),
           ],
