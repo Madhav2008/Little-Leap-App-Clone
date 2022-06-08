b914e46daf81c56b605f225ca36371dd5588d4f2
arora <aroramadhav65@gmail.com>
2022-06-08 20:18:46 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -117,24 +117,27 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                           ],
                         ),
-                        child: ListTile(
-                          title: Text(
-                            'Daily challenges',
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w700,
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: ListTile(
+                            title: Text(
+                              'Daily challenges',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w700,
+                              ),
                             ),
-                          ),
-                          subtitle: Text(
-                            'Earn rewards everyday!',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.grey.shade500,
+                            subtitle: Text(
+                              'Earn rewards everyday!',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.grey.shade500,
+                              ),
                             ),
-                          ),
-                          trailing: Image.asset(
-                            '../assets/images/dailyC.png',
+                            trailing: Image.asset(
+                              '../assets/images/dailyC.png',
+                            ),
                           ),
                         ),
                       ),
