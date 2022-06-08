a4cad07e440aeeb0b9b204c28a8f5b2690b91777
arora <aroramadhav65@gmail.com>
2022-06-08 22:53:34 +0530

    Update

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const primary = Color(0xFF42CB7E);
 const lightPurple = Color(0xFF8D5FC4);
 const lightPink = Color(0xFFF16991);
-const blue = Color(0xFF4AD6FD);
+const lightBlue = Color(0xFF4AD6FD);
