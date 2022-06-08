188a40d5f6d5cf9be5cc751522e6e9f396d7878d
arora <aroramadhav65@gmail.com>
2022-06-08 17:36:00 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -61,7 +61,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       Text(
                         '72',
-                        style: TextStyle(color: Colors.red.shade900),
+                        style: TextStyle(color: Colors.red.shade900,),
                       ),
                     ],
                   ),
