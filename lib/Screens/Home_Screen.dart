02245fdcfe9e5cd174bab98197eea122d0dac721
arora <aroramadhav65@gmail.com>
2022-06-08 17:37:51 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -58,7 +58,8 @@ class _HomeScreenState extends State<HomeScreen> {
                       Icon(
                         Icons.star_rate_rounded,
                         color: Colors.yellow.shade700,
-                      ),SizedBox(),
+                      ),
+                      SizedBox(),
                       Text(
                         '72',
                         style: TextStyle(
