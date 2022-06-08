cba20865733a56b519b87309f0b8469926f2fd00
arora <aroramadhav65@gmail.com>
2022-06-08 13:09:33 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -12,7 +12,9 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-      body: Container(child: Text('hi'),),
+      body: Container(
+        child: Text('hi'),
+      ),
     );
   }
 }
