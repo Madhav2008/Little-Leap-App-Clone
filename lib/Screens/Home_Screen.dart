6ffab47d514e92228e09b003daab7403a04933ba
arora <aroramadhav65@gmail.com>
2022-06-08 19:38:33 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -100,7 +100,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     Container(
                       width: MediaQuery.of(context).size.width - 100,
-                      height: 200,
+                      height: 200,color: Colors.white,
                     ),
                   ],
                 ),
