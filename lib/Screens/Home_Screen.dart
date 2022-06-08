811ae310eb0032614f166d6e50bad2340b298014
arora <aroramadhav65@gmail.com>
2022-06-08 20:17:47 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -138,7 +138,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             trailing: Image.asset(
                               '../assets/images/dailyC.png',
                               width: 100,
-                              height: 200,
+                              height: 100,
                             ),
                           ),
                         ),
