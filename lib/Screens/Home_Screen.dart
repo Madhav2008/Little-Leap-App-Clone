0bdf7883723dc2e412862e88e83c92b396ec9ab7
arora <aroramadhav65@gmail.com>
2022-06-08 13:32:01 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
               color: Colors.white,
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(20),
-                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
               ),
             ),
           ),
