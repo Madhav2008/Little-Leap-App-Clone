ab266e2e31fef0fb0ab80e46258ab5ca4318b1dc
arora <aroramadhav65@gmail.com>
2022-06-08 13:13:29 +0530

    Update

--- lib/Screens/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen.dart
@@ -80,7 +80,7 @@ class _NavigationState extends State<Navigation> {
                   text: 'Menu',
                 ),
               ],
-              selectedIndex: _selectedIndex,
+              selectedIndex: _page,
               onTabChange: (index) {
                 setState(() {
                   _selectedIndex = index;
