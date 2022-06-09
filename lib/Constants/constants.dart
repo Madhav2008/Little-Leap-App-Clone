2d21a8b186bb6189d405e498c3608bb7e5b215b6
arora <aroramadhav65@gmail.com>
2022-06-09 14:54:13 +0530

    Update

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -4,6 +4,6 @@ const primary = Color(0xFF42CB7E);
 var lightPurple = Colors.deepPurple.shade700;
 const lightPink = Color(0xFFF16991);
 const lightBlue = Color(0xFF4AD6FD);
-const purple = Colors.deepPurple.shade800;
+var purple = Colors.deepPurple.shade800;
 const pink = Color(0xFFEE4476);
 const blue = Color(0xFF0099df);
