96e223e238e0500ad9d71c06eb31d9600f336452
arora <aroramadhav65@gmail.com>
2022-06-09 13:10:46 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -205,7 +205,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           padding: const EdgeInsets.all(5),
                           child: ListTile(
                             leading: Image.asset('../assets/images/writing.png',
-                                width: 100, height: 500, fit: BoxFit.cover),
+                                width: 100, height: 500, fit: BoxFit.cover,),
                             title: Text(
                               'Writing',
                               style: TextStyle(
