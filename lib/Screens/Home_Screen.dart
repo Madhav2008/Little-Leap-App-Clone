027b6ec7953f7d54c86f9c01bf60493b7ab0f481
arora <aroramadhav65@gmail.com>
2022-06-09 13:12:54 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -206,7 +206,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: ListTile(
                             leading: Container(
                               width: 200,
-                              height: 200,
+                              height: 700,
                               child: Image.asset(
                                 '../assets/images/writing.png',
                                 // width: 100,
