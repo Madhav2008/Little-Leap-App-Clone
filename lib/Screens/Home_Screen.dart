5aebaa551990aef9bfeef3a758b5246b2216e292
arora <aroramadhav65@gmail.com>
2022-06-10 14:34:35 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             child: Image.asset(
               '../assets/images/My.png',
-              width: 50,
+              width: 80,
               height: 50,
             ),
           ),
