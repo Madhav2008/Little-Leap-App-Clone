1a8316a25db58b8941379b35f1519515bf9752dc
arora <aroramadhav65@gmail.com>
2022-06-10 14:33:32 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -18,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
           onPressed: () {},
           child: Container(
             child: Image.asset(
-              '../assets/images/My.png',
+              '../assets/images/My.png',width: 50,
             ),
             radius: 25,
           ),
