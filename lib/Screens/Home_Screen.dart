7c3640f1d3d3ac705ece4ac478a3e191d403fd34
arora <aroramadhav65@gmail.com>
2022-06-10 14:37:50 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -14,7 +14,8 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: DraggableFab(
-        child: FloatingActionButton(autofocus: true,
+        child: FloatingActionButton(
+          autofocus: true,
           onPressed: () {},
           child: CircleAvatar(
             backgroundImage: AssetImage('../assets/images/My.png'),
