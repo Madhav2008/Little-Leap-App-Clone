3ac833dbd5a95f9f683ab9e8ccbccc09bce157fd
arora <aroramadhav65@gmail.com>
2022-06-10 14:12:27 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -80,7 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 // height: MediaQuery.of(context).size.height / 1.322,
-                height: MediaQuery.of(context).size.height / 1.2,
+                height: MediaQuery.of(context).size.height / 1,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
